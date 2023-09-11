f56011347cb35652ec489336ae8e3e56856899e5
Baysangur Delbiev <111337500+Boisglar@users.noreply.github.com>
2023-09-11 22:17:21 +0300

    задача по список пользователей

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
-
 function App() {
+  const [users, setUsers] = useState([]);
+  const [serch, setSerch] = useState('');
+
+  async function usersFetch() {
+    let res = await fetch('https://reqres.in/api/users');
+    if (res.ok) {
+      res = await res.json();
+      setUsers(res.data);
+    } else {
+      console.error('Ошибка при получении пользователей');
+    }
+  }
+
+  useEffect(() => {
+    usersFetch();
+  }, []);
+
   return (
     <div className="App">
-      <Users />
+      <Users items={users} search={serch} setSearch={setSerch} />
       {/* <Success /> */}
     </div>
   );
